f2f8b6352d0691924ad0aa21f49812e9b38fb71f
markc <mark.c@binance.com>
2024-06-09 09:33:11 +0800

    asset->ass

--- config/generate/genesis/generate-kava-genesis.sh
+++ config/generate/genesis/generate-kava-genesis.sh
@@ -21,7 +21,7 @@ mkdir -p scratch
 
 DATA=./scratch/.kava
 DEST=${DEST:-./config/templates/kava/master/initstate/.kava}
-DENOM=${DENOM:-ukava}
+DENOM=${DENOM:-uass}
 ADDRESSES=./config/common/addresses.json
 
 BINARY="kava --home $DATA"
@@ -91,7 +91,7 @@ sed -i '' 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g' $DATA/con
 sed -i '' 's/enable-unsafe-cors = false/enable-unsafe-cors = true/g' $DATA/config/app.toml
 
 # Set the min gas fee
-sed -i '' 's/minimum-gas-prices = "0ukava"/minimum-gas-prices = "0.001ukava;1000000000akava"/g' $DATA/config/app.toml
+sed -i '' 's/minimum-gas-prices = "0ukava"/minimum-gas-prices = "0uass;0uass"/g' $DATA/config/app.toml
 
 # Disable pruning
 sed -i '' 's/pruning = "default"/pruning = "nothing"/g' $DATA/config/app.toml
@@ -332,7 +332,7 @@ set-app-state evm.accounts
 # setup eip712 allowed messages
 set-app-state evm.params.eip712_allowed_msgs
 # setup enabled precompiles
-set-app-state evm.params.enabled_precompiles
+#set-app-state evm.params.enabled_precompiles
 
 # x/evmutil: enable evm -> sdk conversion pair
 jq '.app_state.evmutil.params.enabled_conversion_pairs = [
